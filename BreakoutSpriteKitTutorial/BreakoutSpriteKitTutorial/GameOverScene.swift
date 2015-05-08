52b1fb0fd90a77830341879a93bb6db1fe97568e
Guilherme Ferreira de Souza <guilherme.souza94@hotmail.com>
2015-05-08 09:01:29 -0300

    Segundo game com SpriteKit

---
+++ BreakoutSpriteKitTutorial/BreakoutSpriteKitTutorial/GameOverScene.swift
@@ -0,0 +1,29 @@
+//
+//  GameOverScene.swift
+//  BreakoutSpriteKitTutorial
+//
+//  Created by Guilherme Ferreira de Souza on 5/6/15.
+//  Copyright (c) 2015 Guilherme Ferreira de Souza. All rights reserved.
+//
+
+import SpriteKit
+
+let GameOverLabelCategoryName = "gameOverLabel"
+
+class GameOverScene: SKScene {
+    var gameWon : Bool = false {
+        // 1.
+        didSet {
+            let gameOverLabel = childNodeWithName(GameOverLabelCategoryName) as! SKLabelNode
+            gameOverLabel.text = gameWon ? "Game Won" : "Game Over"
+        }
+    }
+    
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        if let view = view {
+            // 2.
+            let gameScene = GameScene.unarchiveFromFile("GameScene") as! GameScene
+            view.presentScene(gameScene)
+        }
+    }
+}
